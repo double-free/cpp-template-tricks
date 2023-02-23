e56b30e88890ce65298f9fdb86544a5ee33a8838
Ye Yuan <yuanye_ptr@qq.com>
2023-02-23 23:02:51 +0800

    readability

--- test/TupleTests.cpp
+++ test/TupleTests.cpp
@@ -3,45 +3,44 @@
 
 #include <gtest/gtest.h>
 
-struct A
-{
-};
+struct A {};
 
-struct B: public A
-{
-};
+struct B : public A {};
 
-TEST(TupleTests, IrrelevantTypes)
-{
-    constexpr auto val1 = yy::are_irrelevant_types<int, double>();
-    EXPECT_TRUE(val1);
-    // base and derived
-    constexpr auto val2 = yy::are_irrelevant_types<A, B>();
-    EXPECT_FALSE(val2);
-    // derived and base
-    constexpr auto val3 = yy::are_irrelevant_types<B, A>();
-    EXPECT_FALSE(val3);
-    // same type
-    constexpr auto val4 = yy::are_irrelevant_types<int, int>();
-    EXPECT_FALSE(val4);
+// C is just an alias
+using C = A;
+
+TEST(TupleTests, IrrelevantTypes) {
+  constexpr auto val1 = yy::are_irrelevant_types<int, double>();
+  EXPECT_TRUE(val1);
+  // base and derived
+  constexpr auto val2 = yy::are_irrelevant_types<A, B>();
+  EXPECT_FALSE(val2);
+  // derived and base
+  constexpr auto val3 = yy::are_irrelevant_types<B, A>();
+  EXPECT_FALSE(val3);
+  // same type
+  constexpr auto val4 = yy::are_irrelevant_types<int, int>();
+  EXPECT_FALSE(val4);
 }
 
-TEST(TupleTests, IrrelevantTupleTypes)
-{
-    // empty tuple
-    EXPECT_TRUE(yy::irrelevant_element_types<std::tuple<>>());
-    // tuple with one element
-    EXPECT_TRUE(yy::irrelevant_element_types<std::tuple<int>>());
+TEST(TupleTests, IrrelevantTupleTypes) {
+  // empty tuple
+  EXPECT_TRUE(yy::irrelevant_element_types<std::tuple<>>());
+  // tuple with one element
+  EXPECT_TRUE(yy::irrelevant_element_types<std::tuple<int>>());
 
-    using ValidTuple = std::tuple<std::string, int, double>;
-    EXPECT_TRUE(yy::irrelevant_element_types<ValidTuple>());
+  // tuple with alias types
+  EXPECT_FALSE((yy::irrelevant_element_types<std::tuple<A, C>>()));
 
-    using InvalidTuple = std::tuple<std::string, A, int, B>;
-    EXPECT_FALSE(yy::irrelevant_element_types<InvalidTuple>());
+  using ValidTuple = std::tuple<std::string, int, double>;
+  EXPECT_TRUE(yy::irrelevant_element_types<ValidTuple>());
+
+  using InvalidTuple = std::tuple<std::string, A, int, B>;
+  EXPECT_FALSE(yy::irrelevant_element_types<InvalidTuple>());
 }
 
-TEST(TupleTests, FindType)
-{
+TEST(TupleTests, FindType) {
   // empty tuple, not found
   EXPECT_EQ(0, (yy::element_index<int, std::tuple<>>()));
   // tuple with one element, found
--- yy/tuple/TupleElementCheck.hpp
+++ yy/tuple/TupleElementCheck.hpp
@@ -1,66 +1,49 @@
 #pragma once
 
-#include <tuple>
 #include <cstddef>
+#include <tuple>
 
-namespace yy
-{
-    template <typename T1, typename T2>
-    constexpr bool are_irrelevant_types()
-    {
-        if constexpr (std::is_same_v<T1, T2>)
-        {
-            return false;
-        }
-        if constexpr (std::is_base_of_v<T1, T2> || std::is_base_of_v<T2, T1>)
-        {
-            return false;
-        }
-        return true;
-    }
-    template <typename Tuple, size_t Idx, size_t... OtherIdx>
-    constexpr bool tuple_element_check_helper(std::index_sequence<OtherIdx...>)
-    {
-        return (... && are_irrelevant_types<std::tuple_element_t<Idx, Tuple>,
-                                            std::tuple_element_t<OtherIdx, Tuple>>());
-    }
+namespace yy {
+template <typename T1, typename T2> constexpr bool are_irrelevant_types() {
+  if constexpr (std::is_same_v<T1, T2>) {
+    return false;
+  }
+  if constexpr (std::is_base_of_v<T1, T2> || std::is_base_of_v<T2, T1>) {
+    return false;
+  }
+  return true;
+}
 
-    // As a rule any form of partial template specialisation is not allowed for functions.
-    // We have to wrap it in a class
-    template <typename Tuple, size_t UntilIdx>
-    struct TupleElementChecker
-    {
-        static constexpr bool check()
-        {
-            // Idx >= 2
-            bool ok = tuple_element_check_helper<Tuple, UntilIdx - 1>(
-                std::make_index_sequence<UntilIdx - 1>{});
+// loop through element type at 0 to Idx-1 and compare it with Idx
+template <typename Tuple, size_t Idx, size_t... OtherIdx>
+constexpr bool tuple_element_check_against(std::index_sequence<OtherIdx...>) {
+  return (... && are_irrelevant_types<std::tuple_element_t<Idx, Tuple>,
+                                      std::tuple_element_t<OtherIdx, Tuple>>());
+}
 
-            return ok && TupleElementChecker<Tuple, UntilIdx - 1>::check();
-        }
-    };
-    template <typename Tuple>
-    struct TupleElementChecker<Tuple, 0>
-    {
-        static constexpr bool check()
-        {
-            return true;
-        }
-    };
-    template <typename Tuple>
-    struct TupleElementChecker<Tuple, 1>
-    {
-        static constexpr bool check()
-        {
-            return true;
-        }
-    };
+template <typename Tuple, size_t Index>
+std::enable_if_t<Index == 0, bool> constexpr tuple_element_check_until() {
+  return true;
+}
 
-    // This function check in COMPILE time if all elements in a tuple are irrelevant types
-    // "irrelevant types" mean type A is neither the same with B nor derived from B.
-    template <typename Tuple>
-    constexpr bool irrelevant_element_types()
-    {
-        return TupleElementChecker<Tuple, std::tuple_size_v<Tuple>>::check();
-    }
+template <typename Tuple, size_t Index>
+std::enable_if_t<(Index > 0), bool> constexpr tuple_element_check_until() {
+
+  bool ok = tuple_element_check_against<Tuple, Index>(
+      std::make_index_sequence<Index>{});
+  // if success, continue to check the one before
+  return ok && tuple_element_check_until<Tuple, Index - 1>();
+}
+
+// check if element types in a tuple are relevant
+template <typename Tuple>
+std::enable_if_t<(std::tuple_size_v<Tuple> <= 1),
+                 bool> constexpr irrelevant_element_types() {
+  return true;
+}
+template <typename Tuple>
+std::enable_if_t<(std::tuple_size_v<Tuple> > 1),
+                 bool> constexpr irrelevant_element_types() {
+  return tuple_element_check_until<Tuple, std::tuple_size_v<Tuple> - 1>();
+}
 } // namespace yy
